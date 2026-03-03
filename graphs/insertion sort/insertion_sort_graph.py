0c1800e853f8fa87e0fd57c5a714f0fa7b75ab17
Moumit <143880989+moumitbera@users.noreply.github.com>
2026-03-03 12:14:11 +0530

    Update insertion_sort_graph.py

--- graphs/insertion sort/insertion_sort_graph.py
+++ graphs/insertion sort/insertion_sort_graph.py
@@ -1,31 +1,31 @@
 import matplotlib.pyplot as plt
 
-# --------------------- DATA ---------------------
+# Array size
 
 N = [1000, 2000, 5000, 10000, 20000, 50000]
 
-# ---------------- Comparisons ----------------
+# All comparisons
 sorted_comp = [999, 1999, 4999, 9999, 19999, 49999]
 reverse_comp = [499500, 1999000, 12497500, 49995000, 199990000, 1249975000]
 random_comp = [248288, 1004094, 6242496, 25077297, 100175744, 624949001]
 duplicate_comp = [237236, 952758, 5942234, 23774823, 95014971, 593828027]
 nearly_comp = [34132, 122705, 764749, 3109850, 12459258, 77414619]
 
-# ---------------- Shifts ----------------
+# Shift details
 sorted_shift = [0, 0, 0, 0, 0, 0]
 reverse_shift = [499500, 1999000, 12497500, 49995000, 199990000, 1249975000]
 random_shift = [247295, 1002101, 6237505, 25067305, 100155575, 624899012]
 duplicate_shift = [236239, 950761, 5937238, 23764827, 94994975, 593778030]
 nearly_shift = [33133, 120706, 759750, 3099851, 12439259, 77364620]
 
-# ---------------- Runtime (ms) ----------------
+# Runtime data
 sorted_time = [0.016, 0.015, 0.016, 0.074, 0.150, 0.374]
 reverse_time = [1.304, 4.337, 18.097, 69.263, 272.022, 1700.455]
 random_time = [1.040, 2.727, 10.000, 34.422, 136.546, 849.583]
 duplicate_time = [0.992, 2.561, 9.565, 33.194, 129.383, 806.148]
 nearly_time = [0.173, 0.602, 2.324, 5.880, 18.295, 105.793]
 
-# ================= GRAPH 1 =================
+# Make graph 1
 plt.figure()
 plt.plot(N, sorted_comp, marker='o', label="Sorted")
 plt.plot(N, reverse_comp, marker='o', label="Reverse Sorted")
@@ -41,7 +41,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-# ================= GRAPH 2 =================
+# Make graph 2
 plt.figure()
 plt.plot(N, sorted_shift, marker='o', label="Sorted")
 plt.plot(N, reverse_shift, marker='o', label="Reverse Sorted")
@@ -57,7 +57,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-# ================= GRAPH 3 =================
+# Make graph 3
 plt.figure()
 plt.plot(N, sorted_time, marker='o', label="Sorted")
 plt.plot(N, reverse_time, marker='o', label="Reverse Sorted")
